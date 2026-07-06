252b1fa6b22bea6e6014b0e2b8b19f0491c76c53
qwc-ch <zzzzzzxx2022@163.com>
2026-07-06 11:01:25 +0800

    feat: redirect unauthenticated users to /login

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,5 +1,14 @@
 import indexHtml from '../../static/index.html?raw';
+import type { RequestEvent } from '@sveltejs/kit';
 
-export const GET = () => new Response(indexHtml, {
-    headers: { 'content-type': 'text/html' },
-});
+export const GET = (event: RequestEvent) => {
+    if (!event.locals.user) {
+        return new Response(null, {
+            status: 302,
+            headers: { location: '/login' },
+        });
+    }
+    return new Response(indexHtml, {
+        headers: { 'content-type': 'text/html' },
+    });
+};
